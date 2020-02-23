c846933b5f966ff3c5d1b712b3d9c313577a9e7a
JosteveGit <adekanbijosteve@gmail.com>
2020-02-23 22:38:30 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/example/firebase/flutter/com/open_source_projects/MainActivity.kt
@@ -0,0 +1,12 @@
+package example.firebase.flutter.com.open_source_projects
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
